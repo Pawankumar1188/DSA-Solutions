111235ab0c86fbaa8203f187942d3f06abb241c4
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-06 15:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.38%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,63 +1,61 @@
 class Solution {
 public:
-    int ans=0;
-    
-    void bfs(vector<vector<int>>& grid,vector<int>&vis,vector<pair<int,int>>&dir,queue<pair<int,int>>&q){
-        int m=grid[0].size();
-        
-        
-        
-        while(!q.empty()){
-            
-            int s=q.size();
-            bool changed=false;
-            for(int i=0;i<s;i++){
-                auto node=q.front();
-                q.pop();
-                for(auto d:dir){
-                    int newx=d.first+node.first;
-                    int newy=d.second+node.second;
-                    if(newx>=0&&newx<grid.size()&&newy>=0&&newy<grid[0].size()&&vis[m*newx+newy]==0&&grid[newx][newy]==1){
-                        grid[newx][newy]=2;
-                        q.push({newx,newy});
-                        changed=true;
-                        vis[m*newx+newy] = 1;
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
+        queue<pair<int, int>> q;
+        int fresh = 0;
+
+        // Push all rotten oranges into the queue
+        // Count fresh oranges
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        // If there are no fresh oranges
+        if (fresh == 0) return 0;
+
+        vector<pair<int, int>> dir = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        int minutes = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
+            bool rottenThisMinute = false;
+
+            while (sz--) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (auto [dx, dy] : dir) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    if (nx >= 0 && nx < n &&
+                        ny >= 0 && ny < m &&
+                        grid[nx][ny] == 1) {
+
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.push({nx, ny});
+                        rottenThisMinute = true;
                     }
                 }
             }
-            if(changed){
-                ans++;
-            }
 
+            if (rottenThisMinute)
+                minutes++;
+        }
 
-        }
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<int>vis(n*m,0);
-        queue<pair<int,int>>q;
-        vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    vis[m*i + j] = 1;
-                    q.push({i,j});
-                    
-                }
-            }
-        }
-        
-        bfs(grid,vis,dir,q);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                    
-                }
-            }
-        }
-        return ans;
+        return (fresh == 0) ? minutes : -1;
     }
 };
