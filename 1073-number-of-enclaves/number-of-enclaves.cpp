ed566802f1079661a78c55251719812481b3258d
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-07 16:26:40 +0530

    Time: 187 ms (6.51%) | Memory: 51.6 MB (7.68%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     void dfs(int i,int j,vector<vector<int>>& board){
-        
-
         board[i][j]=2;
         vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
         for(auto [x,y]:dir){
@@ -12,25 +10,23 @@ public:
         } 
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>vis(m,vector<int>(n));
+        
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(grid[0][i]==1&&!vis[0][i]){
+            if(grid[0][i]==1){
                 dfs(0,i,grid);
             }
-            if(grid[m-1][i]==1&&!vis[m-1][i]){
+            if(grid[m-1][i]==1){
                 dfs(m-1,i,grid);
             }
-            
         }
         for(int i=0;i<m;i++){
-            if(grid[i][0]==1&&!vis[i][0]){
+            if(grid[i][0]==1){
                 dfs(i,0,grid);
             }
-            if(grid[i][n-1]==1&&!vis[i][n-1]){
+            if(grid[i][n-1]==1){
                 dfs(i,n-1,grid);
             }
         }
