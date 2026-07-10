aaf4b8ecfb51d946288a137e5e45fbd935f4bef2
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-10 12:10:48 +0530

    Time: 15 ms (72.32%) | Memory: 24.4 MB (73.41%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-        vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
-        queue<pair<int,int>>q;
-        if(grid[0][0]==0){
-            q.push({0,0});
-            dist[0][0]=0;
-        }
-        
-        
-        while(!q.empty()){
-            auto [i,j]=q.front();
+        int n = grid.size();
+
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+            return -1;
+
+        vector<pair<int,int>> dir = {
+            {1,0}, {-1,0}, {0,1}, {0,-1},
+            {1,1}, {1,-1}, {-1,1}, {-1,-1}
+        };
+
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},1});
+        grid[0][0] = 1;
+
+        while (!q.empty()) {
+            auto [cell, len] = q.front();
             q.pop();
-            for(auto [i_,j_]:dir){
-                int ni=i+i_;
-                int nj=j+j_;
-                if(ni<0||ni>=n||nj<0||nj>=n||grid[ni][nj]==1){
+
+            auto [i, j] = cell;
+
+            if (i == n-1 && j == n-1)
+                return len;
+
+            for (auto [dx, dy] : dir) {
+                int ni = i + dx;
+                int nj = j + dy;
+
+                if (ni < 0 || ni >= n || nj < 0 || nj >= n)
                     continue;
-                }
-                if(dist[ni][nj]>1+dist[i][j]){
-                    dist[ni][nj]=1+dist[i][j];
-                    q.push({ni,nj});
-                    cout<<ni<<" "<<nj<<endl;
-                }
+
+                if (grid[ni][nj] == 1)
+                    continue;
+
+                grid[ni][nj] = 1; 
+                q.push({{ni, nj}, len + 1});
             }
         }
-        if(dist[n-1][n-1]==INT_MAX) return -1;
-        return dist[n-1][n-1]+1;
+
+        return -1;
     }
 };
