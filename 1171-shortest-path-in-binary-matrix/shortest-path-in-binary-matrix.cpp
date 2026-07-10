201db010f75ca3818e3967d19bf95298e3715f9a
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-10 12:03:46 +0530

    Time: 689 ms (5.05%) | Memory: 25.2 MB (58.18%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        queue<pair<int,int>>q;
+        if(grid[0][0]==0){
+            q.push({0,0});
+            dist[0][0]=0;
+        }
+        
+        
+        while(!q.empty()){
+            auto [i,j]=q.front();
+            q.pop();
+            for(auto [i_,j_]:dir){
+                int ni=i+i_;
+                int nj=j+j_;
+                if(ni<0||ni>=n||nj<0||nj>=n||grid[ni][nj]==1){
+                    continue;
+                }
+                if(grid[ni][nj]==0&&dist[ni][nj]>1+dist[i][j]){
+                    dist[ni][nj]=1+dist[i][j];
+                    q.push({ni,nj});
+                    cout<<ni<<" "<<nj<<endl;
+                }
+            }
+        }
+        if(dist[n-1][n-1]==INT_MAX) return -1;
+        return dist[n-1][n-1]+1;
+    }
+};
