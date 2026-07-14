2751a3fb3d24dcb255dd222b1e97205914879b9f
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-14 19:56:44 +0530

    Time: 222 ms (5.25%) | Memory: 44.1 MB (62.92%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,53 @@
-class Solution {
-public:
-    void dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
-        vis[node]=1;
-        for(auto adjnode:adj[node]){
-            if(!vis[adjnode]){
-                dfs(adjnode,adj,vis);
-            }
+class DSU{
+    vector<int>parent,size;
+    public:
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
     }
+    int ultimateP(int u){
+        if(u==parent[u]){
+            return u;
+        }
+        return parent[u]=ultimateP(parent[u]);
+    }
+    void unionbysize(int u,int v){
+        int ultpu=ultimateP(u);
+        int ultpv=ultimateP(v);
+        if(ultpu == ultpv) return;
+        if(size[ultpu]<size[ultpv]){
+            parent[ultpu]=ultpv;
+            size[ultpv]+=size[ultpu];
+        }
+        else{
+            parent[ultpv]=ultpu;
+            size[ultpu]+=size[ultpv];
+        }
+    }
+    int sz(){
+        set<int>s;
+        for(int i=0;i<parent.size();i++){
+            s.insert(ultimateP(i));
+        }
+        return s.size();
+    }
+};
+class Solution {
+public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1){
             return -1;
         }
-        vector<vector<int>>adj(n);
-        for(int i=0;i<connections.size();i++){
-            adj[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(connections[i][0]);
-        }
-        int cnt=0;
-        vector<int>vis(n);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                cnt++;
-            }
 
+        DSU ds(n);
+        for(int i=0;i<connections.size();i++){
+            ds.unionbysize(connections[i][0],connections[i][1]);
+            cout<<i<<endl;
         }
-        return cnt-1;
+        return ds.sz()-1;
     }
 };
