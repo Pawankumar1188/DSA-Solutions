6d71375d083eac55e7f07bc6774e0301840d5b49
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-07-29 14:11:40 +0530

    Time: 4 ms (77.05%) | Memory: 12.4 MB (98.54%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -4,9 +4,9 @@ public:
         int n=grid.size();
         vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        // vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
         pq.push({grid[0][0],{0,0}});
-        dist[0][0]=grid[0][0];
+        // dist[0][0]=grid[0][0];
         int ans=0;
         while(!pq.empty()){
             int w=pq.top().first;
@@ -14,17 +14,18 @@ public:
             int y=pq.top().second.second;
             
             ans=max(ans,w);
-            cout<<ans<<endl;
+            // cout<<ans<<endl;
             pq.pop();
             for(auto [p,q]:dir){
                 int nx=x+p;
                 int ny=y+q;
-                if(nx>=0&&nx<n&&ny>=0&&ny<n&&dist[nx][ny]==INT_MAX){
+                if(nx>=0&&nx<n&&ny>=0&&ny<n&&grid[nx][ny]!=INT_MAX){
                     pq.push({grid[nx][ny],{nx,ny}});
-                    dist[nx][ny]=grid[nx][ny];
                     if(nx==n-1&&ny==n-1){
-                        return max(ans,dist[nx][ny]);
+                        return max(ans,grid[nx][ny]);
                     }
+                    grid[nx][ny]=INT_MAX;
+                    
                 }
             }
         }
