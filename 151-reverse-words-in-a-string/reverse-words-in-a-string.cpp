dadd4561539ab9731e2d2a9e130b6bd1817dc958
Pawankumar1188 <143873078+Pawankumar1188@users.noreply.github.com>
2026-08-02 10:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (88.38%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        
-        int i=0;
-        int j=n-1;
-        string ans="";
-        while(s[i]==' '){
-            i++;
-        }
-        while(s[j]==' '){
-            j--;
-        }
-        int l=j;
-        int r=j;
-        while(l>=i){
-            if(s[l]==' '&&r==l){
-                l--;
-                r--;
-            }
-            else if(s[l]==' '&&r-l>=1){
-                ans=ans+s.substr(l+1,r-l)+' ';
-                l--;
-                r=l;
-            }
-            else{
-                l--;
-            }
-            
-            
-        }
-        if(r-l>0){
-            ans=ans+s.substr(l+1,r-l);
-        }
-        return ans;
+        int n = s.size();
 
+        int i = 0, j = n - 1;
+
+        while (i < n && s[i] == ' ')
+            i++;
+
+        while (j >= 0 && s[j] == ' ')
+            j--;
+
+        string ans = "";
+
+        int r = j;
+        int l = j;
+
+        while (l >= i) {
+
+            // Skip consecutive spaces
+            while (l >= i && s[l] == ' ')
+                l--;
+
+            if (l < i)
+                break;
+
+            r = l;
+
+            // Find beginning of current word
+            while (l >= i && s[l] != ' ')
+                l--;
+
+            ans += s.substr(l + 1, r - l);
+
+            if (l >= i)
+                ans += ' ';
+        }
+
+        return ans;
     }
 };
